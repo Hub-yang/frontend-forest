03e6a0f8f20422042f2be4bed2c4bb9fa005eb8d
Mocheng <78254474+Mochenghualei@users.noreply.github.com>
2023-02-09 16:32:46 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ npm run build
 
 # cd 到构建输出的目录下 
 cd dist
-
 echo > .nojekyll
+
 
 git init
 git checkout -B main
@@ -15,6 +15,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Mochenghualei/my-personal-website.git main:gh-pages
+git push -f https://github.com/Mochenghualei/my-personal-website.git main:gh-pages
 
 cd -
