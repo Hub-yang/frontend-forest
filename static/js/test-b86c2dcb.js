7d596a9624ed467cbf4301324f3f36609a7d0483
Mocheng <78254474+Mochenghualei@users.noreply.github.com>
2023-04-12 11:27:54 +0800

    deploy

--- static/js/test-b86c2dcb.js
+++
@@ -1,1 +0,0 @@
-const t="/my-personal-website/static/flac/test-bbcc3ac1.flac";export{t as default};
--- static/js/test-legacy-4b40f43d.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(t,e){"use strict";return{execute:function(){t("default","/my-personal-website/static/flac/test-bbcc3ac1.flac")}}}));
