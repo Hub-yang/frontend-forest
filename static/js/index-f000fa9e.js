f9d4e400109ecdae7198e77cd16e405f20ca6162
Mocheng <78254474+Mochenghualei@users.noreply.github.com>
2023-04-13 17:23:21 +0800

    deploy

---
+++ static/js/index-f000fa9e.js
@@ -0,0 +1,1 @@
+import{x as o,L as a,e as s,u as c}from"./index-0884881c.js";import{d as r}from"./index-17cfbf37.js";const l=e=>e,i={prefix:Math.floor(Math.random()*1e4),current:0},u=Symbol("elIdInjection"),p=e=>{const t=o(u,i),n=a("namespace",r);return s(()=>c(e)||`${n.value}-id-${t.prefix}-${t.current++}`)};export{l as m,p as u};
