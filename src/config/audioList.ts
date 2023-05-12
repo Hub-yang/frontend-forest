77d35a0c5613b2ae44e936e0a0c47a101f2d71fd
mocheng <18830279823@163.com>
2023-05-12 16:35:40 +0800

    update: 更新播放列表

--- src/config/audioList.ts
+++ src/config/audioList.ts
@@ -1,48 +1,4 @@
-// type audiolist = { [name: string]: any }
-// const audioList: audiolist[] = []
-
-// // 获取歌曲名和作者
-// function autoReading(audioName: string): { name: string, artist: string } {
-//   const audioArr = audioName.split("-")
-//   const name = audioArr[1] ? audioArr[1].split(".")[0] : "--"
-//   const artist = audioArr[0] || "--"
-//   return {
-//     name,
-//     artist
-//   }
-// }
-
-// // 返回绝对路径
-// function absPath(path: string): string {
-//   return new URL(path, import.meta.url).href
-// }
-
-
-// // 读取audios文件夹
-// const modules = import.meta.glob('../assets/audios/*')
-// for (const path in modules) {
-//   const fileName = path.replace(/..\/assets.audios\//, '')
-//   audioList.push({
-//     name: autoReading(fileName).name,
-//     artist: autoReading(fileName).artist,
-//     // url: absPath(path),
-//     url: "http://rszfze984.hn-bkt.clouddn.com/G.E.M.%E9%82%93%E7%B4%AB%E6%A3%8B-%E5%8F%A6%E4%B8%80%E4%B8%AA%E7%AB%A5%E8%AF%9D.flac",
-//   })
-// }
-
-// // 添加歌词与封面
-// audioList.forEach((item) => {
-//   if (item.name) {
-//     item.cover = absPath(`../assets/cover/${item.name}.jpg`)
-//     item.lrc = absPath(`../assets/lrc/${item.name}.lrc`)
-//   }
-// })
-
-// export {
-//   audioList
-// } 
-
-type Audio = {
+interface Audio {
   name?: string,
   artist?: string,
   url?: string,
@@ -51,25 +7,18 @@ type Audio = {
 }
 
 export const audioList: Audio[] = [
-  // {
-  //   name: "另一个童话",
-  //   artist: "G.E.M.邓紫棋",
-  //   url: "http://mochenghualei.com.cn/G.E.M.%20%E9%82%93%E7%B4%AB%E6%A3%8B-%E5%8F%A6%E4%B8%80%E4%B8%AA%E7%AB%A5%E8%AF%9D.flac",
-  //   cover: "http://rszfvyav9.hn-bkt.clouddn.com/covers/%E5%8F%A6%E4%B8%80%E4%B8%AA%E7%AB%A5%E8%AF%9D.jpg",
-  //   lrc: "http://rszfvyav9.hn-bkt.clouddn.com/lrc/%E5%8F%A6%E4%B8%80%E4%B8%AA%E7%AB%A5%E8%AF%9D.lrc"
-  // },
+  {
+    name: "黑色泡沫",
+    artist: "林俊杰",
+    url: "http://mochenghualei.com.cn/audios/%E6%9E%97%E4%BF%8A%E6%9D%B0-%E9%BB%91%E8%89%B2%E6%B3%A1%E6%B2%AB.flac",
+    cover: "http://mochenghualei.com.cn/covers/jj-%E9%87%8D%E6%8B%BE%E5%BF%AB%E4%B9%90.jpeg",
+    lrc: "http://mochenghualei.com.cn/lrcs/%E6%9E%97%E4%BF%8A%E6%9D%B0-%E9%BB%91%E8%89%B2%E6%B3%A1%E6%B2%AB.lrc"
+  },
   {
     name: "天空没有极限",
-    artist: "G.E.M.邓紫棋",
+    artist: "邓紫棋",
     url: "http://mochenghualei.com.cn/audios/G.E.M.%E9%82%93%E7%B4%AB%E6%A3%8B-%E5%A4%A9%E7%A9%BA%E6%B2%A1%E6%9C%89%E6%9E%81%E9%99%90.flac",
     cover: "http://mochenghualei.com.cn/covers/%E5%A4%A9%E7%A9%BA%E6%B2%A1%E6%9C%89%E6%9E%81%E9%99%90.jpg",
     lrc: "http://mochenghualei.com.cn/lrcs/%E5%A4%A9%E7%A9%BA%E6%B2%A1%E6%9C%89%E6%9E%81%E9%99%90.lrc"
   },
-  {
-    name: "再见深海 (微亮的瞬间)",
-    artist: "唐汉霄",
-    url: "http://mochenghualei.com.cn/audios/%E5%94%90%E6%B1%89%E9%9C%84-%E5%86%8D%E8%A7%81%E6%B7%B1%E6%B5%B7%20%28%E5%BE%AE%E4%BA%AE%E7%9A%84%E7%9E%AC%E9%97%B4%29.flac",
-    cover: "http://mochenghualei.com.cn/covers/%E5%86%8D%E8%A7%81%E6%B7%B1%E6%B5%B7%20%28%E5%BE%AE%E4%BA%AE%E7%9A%84%E7%9E%AC%E9%97%B4%29.jpg",
-    lrc: "http://mochenghualei.com.cn/lrcs/%E5%86%8D%E8%A7%81%E6%B7%B1%E6%B5%B7%20%28%E5%BE%AE%E4%BA%AE%E7%9A%84%E7%9E%AC%E9%97%B4%29.lrc"
-  },
-] 
+]
