b0a59ac39448bc8727b695b8e61006998d4bdf0f
HuberyYang <18830279823@163.com>
2023-10-23 16:23:30 +0800

    deploy

--- static/js/index-71741f3e.js
+++
@@ -1,1 +0,0 @@
-import{x as o,L as a,e as s,u as c}from"./index-39c95104.js";import{d as r}from"./index-7c056b7d.js";const l=e=>e,i={prefix:Math.floor(Math.random()*1e4),current:0},u=Symbol("elIdInjection"),p=e=>{const t=o(u,i),n=a("namespace",r);return s(()=>c(e)||`${n.value}-id-${t.prefix}-${t.current++}`)};export{l as m,p as u};
